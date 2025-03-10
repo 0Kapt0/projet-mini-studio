887633fb495cb047dbde8476e0153562ccfff4b7
0Kapt0 <lorenzo.capitaine@gmail.com>
2025-03-10 17:32:33 +0100

    push game.cpp changement a faire dans le gmae et plus le main

--- projet-mini-studio/scr/Game.cpp
+++ projet-mini-studio/scr/Game.cpp
@@ -1,6 +1,11 @@
 #include "../include/Game.hpp"
 #include "../include/Map.hpp"
 #include "../include/Menu.hpp"
+#include "../include/Player.hpp"
+#include "../include/Enemy.hpp"
+#include "../include/RangedEnemy.hpp"
+#include "../include/TileSelector.hpp"
+
 #include <iostream>
 
 using namespace sf;
@@ -14,9 +19,7 @@ enum class GameState {
     GameOver
 };
 
-Game::Game(){
-    
-}
+Game::Game() {}
 
 Game::~Game() {
     cout << "Le jeu est détruit\n";
@@ -25,62 +28,120 @@ Game::~Game() {
 void Game::run() {
     RenderWindow window(VideoMode(1920, 1080), "Map Editor");
     window.setFramerateLimit(60);
+
     // Définir l'état initial du jeu
     GameState currentState = GameState::Menu;
+
+    // Instanciation des objets
     Map map("assets/tileset/Tileset_Grass.png", "assets/map/Lobby.txt");
     Menu menu;
+    TileSelector tileSelector("assets/tileset/Tileset_Grass.png", 32);
+    Player player(Vector2f(50, 50), Color::Red, map);
+    Enemy enemy(Vector2f(50, 50), Color::Blue);
+    RangedEnemy rangedEnemy(Vector2f(50, 50), Color::Yellow);
+
+    bool collisionMode = false;
+    Clock clock;
+
     while (window.isOpen()) {
         Event event;
         while (window.pollEvent(event)) {
             if (event.type == Event::Closed)
                 window.close();
-            // Gérer les événements en fonction de l'état actuel du jeu
+
+            // Gestion des événements en fonction de l'état actuel
             switch (currentState) {
             case GameState::Menu:
                 if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
                     if (menu.editSprite.getGlobalBounds().contains(window.mapPixelToCoords(Mouse::getPosition(window)))) {
                         currentState = GameState::Editor;
                     }
-                   
                 }
                 break;
+
             case GameState::Playing:
-                if (event.type == Event::MouseButtonPressed) {
-                }
+                player.handleInput(event, window);
                 break;
+
             case GameState::Editor:
+                player.handleInput(event, window);
+                tileSelector.handleEvent(event, window);
+
                 if (event.type == Event::MouseButtonPressed) {
-                    int tileIndex = 1;
-                    map.handleClick(event.mouseButton.x, event.mouseButton.y, tileIndex);
+                    int selectedTile = tileSelector.getSelectedTile();
+                    int x = event.mouseButton.x;
+                    int y = event.mouseButton.y;
+
+                    // Clic gauche : placer une tuile
+                    if (event.mouseButton.button == Mouse::Left && selectedTile != -1) {
+                        map.handleClick(x, y, selectedTile);
+                    }
+                    // Clic droit : effacer une tuile
+                    else if (event.mouseButton.button == Mouse::Right) {
+                        map.handleClick(x, y, 0);
+                    }
+                }
+
+                // Touche "C" pour activer/désactiver la collision
+                if (event.type == Event::KeyPressed && event.key.code == Keyboard::C) {
+                    tileSelector.toggleCollision();
                 }
                 break;
+
             case GameState::Pause:
                 break;
+
             case GameState::GameOver:
                 break;
             }
         }
-        // Gérer le rendu en fonction de l'état
+
+        // Gestion du temps
+        float deltaTime = clock.restart().asSeconds();
+
+        // Mise à jour selon l'état du jeu
+        if (currentState == GameState::Playing || currentState == GameState::Editor) {
+            player.update(deltaTime);
+            rangedEnemy.update(deltaTime);
+            enemy.update(0.016f);
+        }
+
         window.clear();
 
+        // Rendu en fonction de l'état du jeu
         switch (currentState) {
         case GameState::Menu:
             menu.draw(window);
             break;
+
         case GameState::Playing:
+            map.draw(window);
+            player.draw(window);
+            enemy.draw(window);
+            rangedEnemy.draw(window);
+            rangedEnemy.drawProjectiles(window);
             break;
+
         case GameState::Editor:
             map.draw(window);
+            tileSelector.draw(window);
+            player.draw(window);
+            enemy.draw(window);
+            rangedEnemy.draw(window);
+            rangedEnemy.drawProjectiles(window);
             break;
+
         case GameState::Pause:
             break;
+
         case GameState::GameOver:
             break;
         }
 
         window.display();
     }
-    // Sauvegarder l'état final de la carte si nécessaire
+
+    // Sauvegarde de la carte en quittant l'éditeur
     if (currentState == GameState::Editor) {
         map.saveMap("assets/map/Lobby.txt");
     }
