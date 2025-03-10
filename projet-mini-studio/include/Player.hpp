1733c6490b2a0fe13d09878020e4f68bdfc3b26d
JulienRdgs <julien.rodrigues.onze@gmail.com>
2025-03-10 12:05:15 +0100

    dash

--- projet-mini-studio/include/Player.hpp
+++ projet-mini-studio/include/Player.hpp
@@ -7,6 +7,13 @@ private:
 	Vector2f velocity;
 	bool canJump = true;
 	int jumpNum = 0;
+	bool canDash = true;
+	bool dashing = false;
+	Vector2f dashDirection;
+	char lastInputDirection = 'N';
+	float dashDuration = 0;
+	float dashCooldown = 0.8;
+	float dashTimer = 0;
 public:
 	Player();
 	Player(const Vector2f& size, const Color& color);
--- projet-mini-studio/scr/Player.cpp
+++ projet-mini-studio/scr/Player.cpp
@@ -18,33 +18,64 @@ Player::~Player()
 
 void Player::update(float dt)
 {
-	if (!Keyboard::isKeyPressed(Keyboard::Up) && jumpNum < 2)
-	{
-		canJump = true;
+	if (!dashing) {
+		dashTimer += dt;
+		if (dashTimer >= dashCooldown && getSprite().getPosition().y > 200) canDash = true;
+
+		if (!Keyboard::isKeyPressed(Keyboard::Z) && jumpNum < 2)
+		{
+			canJump = true;
+		}
+
+		velocity.y += 14.8f;
+
+		if (getSprite().getPosition().y > 200)
+		{
+			canJump = true;
+			jumpNum = 0;
+			velocity.y = 0;
+		}
+		if (!Keyboard::isKeyPressed(Keyboard::Q) && !Keyboard::isKeyPressed(Keyboard::D))
+			velocity.x = 0;
+
+		if (Keyboard::isKeyPressed(Keyboard::Q)) {
+			velocity.x = -speed;
+			lastInputDirection = 'L'; 
+		}
+		if (Keyboard::isKeyPressed(Keyboard::D)) {
+			velocity.x = speed;
+			lastInputDirection = 'R';
+		}
+		if (Keyboard::isKeyPressed(Keyboard::Z) && canJump)
+		{
+			velocity.y = -speed;
+			jumpNum++;
+			canJump = false;
+		}
+		if (Keyboard::isKeyPressed(Keyboard::S) && getSprite().getPosition().y < 200)
+			velocity.y += speed;
+		getSprite().move(velocity * dt);
+
+		if (Keyboard::isKeyPressed(Keyboard::Space) && canDash) {
+			dashing = true;
+			canDash = false;
+			dashTimer = 0;
+			velocity.y = 0;
+			
+			switch (lastInputDirection) {
+				case('L'): dashDirection.x = -speed * 3.f; break;
+				case('R'): dashDirection.x = speed * 3.f; break;
+			}
+			//dashDirection = velocity * 3.f; // si on veut un dash dans toutes les directions
+		}
 	}
 
-	velocity.y += 14.8f;
-
-	if (getSprite().getPosition().y > 200)
-	{
-		canJump = true;
-		jumpNum = 0;
-		velocity.y = 0;
+	else { //(dashing)
+		dashDuration += dt;
+		if (dashDuration >= 0.2) {
+			dashing = false;
+			dashDuration = 0;
+		}
+		getSprite().move(dashDirection.x * dt, 0);
 	}
-	if (!Keyboard::isKeyPressed(Keyboard::Left) && !Keyboard::isKeyPressed(Keyboard::Right))
-		velocity.x = 0;
-	
-	if (Keyboard::isKeyPressed(Keyboard::Left))
-		velocity.x = -speed;
-	if (Keyboard::isKeyPressed(Keyboard::Right))
-		velocity.x = speed;
-	if (Keyboard::isKeyPressed(Keyboard::Up) && canJump)
-	{
-		velocity.y = -speed;
-		jumpNum++;
-		canJump = false;
-	}
-	if (Keyboard::isKeyPressed(Keyboard::Down) && getSprite().getPosition().y < 200)
-		velocity.y += speed;
-	getSprite().move(velocity * dt);
 }
